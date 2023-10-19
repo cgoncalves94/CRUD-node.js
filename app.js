d1b38504dcaf8411b73ab4b96421a074e8ba3b4b
Cesar Augusto Goncalves <107410135+cgoncalves94@users.noreply.github.com>
2023-10-19 22:07:29 +0100

    Pushing my app files

---
+++ app.js
@@ -0,0 +1,49 @@
+
+// Importing the express
+const express = require('express');
+
+// Creating the express app
+const app = express();
+
+// Importing the MURL
+require('dotenv').config();
+
+// Importing the mongoose
+const mongoose = require('mongoose');
+
+
+// Importing the body-parser
+const bodyParser = require('body-parser');
+
+// Importing the postsRoute
+const postsRoute = require('./routes/posts');
+
+// bodyParser middleware
+app.use(bodyParser.json());
+
+// Using the postsRoute middleware
+app.use('/posts', postsRoute);
+
+// Homepage route
+app.get('/', (req, res) => {
+    res.send('Homepage');
+});
+
+// Log the connection string for debugging purposes (remove this once verified!)
+console.log("Connecting to:", process.env.DB_CONNECTOR);
+
+// Mongoose connection to MongoDB
+mongoose.connect(process.env.DB_CONNECTOR)
+    .then(() => {
+        console.log('connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB', err);
+    });
+
+
+// Server listening
+app.listen(3000, () => {
+    console.log('server is up and running');
+});
+
---
+++ routes/posts.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const router = express.Router();
+
+// Importing the Post model
+const Post = require('../models/Post');
+
+// POST (create data)
+router.post('/', async (req, res) => {
+
+    const postData = new Post({
+        user:req.body.user,
+        title:req.body.title,
+        text:req.body.text,
+        hashtag:req.body.hashtag,
+        location:req.body.location,
+        url:req.body.url,
+    });
+    // try to insert...
+    try{
+        const savedPost = await postData.save();
+        res.send(savedPost);
+    }
+    catch(err){
+        res.send({message: err});
+    }
+});
+
+// GET (read all data)
+router.get('/', async (req, res) => {
+    try{
+        const getPosts = await Post.find.limit(10);
+        res.send(getPosts);
+    }
+    catch(err){
+        res.json({message: err});
+    }
+});
+
+// GET (read by ID)
+router.get('/:postId', async (req, res) => {
+    try{
+        const getPostByID = await Post.findById(req.params.postId);
+        res.send(getPostByID);
+    }
+    catch(err){
+        res.json({message: err});
+    }
+});
+
+
+// PATCH (update data)
+router.patch('/:postId', async (req, res) => {
+    try{
+        const updatePost = await Post.updateOne(
+            {_id: req.params.postId},
+            {$set: 
+                {
+                    user:req.body.user,
+                    title:req.body.title,
+                    text:req.body.text,
+                    hashtag:req.body.hashtag,
+                    location:req.body.location,
+                    url:req.body.url,
+                }
+            }
+        );
+        res.send(updatePost);
+    }
+    catch(err){
+        res.send({message: err});
+    }
+});
+
+// DELETE (delete data)
+router.delete('/:postId', async (req, res) => {
+    try{
+        const deletePostbyID = await Post.deleteOne({_id: req.params.postId});
+        res.send(deletePostbyID);
+    }
+    catch(err){
+        res.send({message: err});
+    }
+});
+
+
+router.post('/', async (req, res) => {
+    console.log(req.body);
+});
+
+module.exports = router;
