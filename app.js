3b2ccac9dfab9537c78b68dcaef4a5506c23f03c
Cesar Augusto Goncalves <107410135+cgoncalves94@users.noreply.github.com>
2023-10-19 22:50:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-
 // Importing the express
 const express = require('express');
-
 // Creating the express app
 const app = express();
-
 // Importing the MURL
 require('dotenv').config();
-
 // Importing the mongoose
 const mongoose = require('mongoose');
-
-
 // Importing the body-parser
 const bodyParser = require('body-parser');
-
 // Importing the postsRoute
 const postsRoute = require('./routes/posts');
+
 
 // bodyParser middleware
 app.use(bodyParser.json());
@@ -37,8 +31,7 @@ mongoose.connect(process.env.DB_CONNECTOR)
     .catch((err) => {
         console.error('Error connecting to MongoDB', err);
     });
-
-
+               
 // Server listening
 app.listen(3000, () => {
     console.log('server is up and running');
